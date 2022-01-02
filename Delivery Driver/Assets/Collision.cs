f54e2a221f291a2ba632ccbb580750161a70d3f0
Franklin Moon <42459707+OperationFman@users.noreply.github.com>
2022-01-03 10:52:04 +1100

    Import DD assets

--- Delivery Driver/Assets/Collision.cs
+++ Delivery Driver/Assets/Collision.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    void OnCollisionEnter2D(Collision2D other) {
-        Debug.Log("havatthee");
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Boost!");
     }
 }
